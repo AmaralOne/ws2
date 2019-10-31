567a10ec19c97f7152074ff937276119219a4474
Alexssandercs <alexssander.sousa@ovg.org.br>
2019-10-31 17:28:42 -0300

    PontoC

--- WebServiceE/DAL/Produto.cs
+++ WebServiceE/DAL/Produto.cs
@@ -38,10 +38,10 @@ namespace WebServiceE.DAL
         public string adiconarProduto(Produt p)
         {
             string retorno = "0";
-
+            string valorPonto = p.valor.ToString().Replace(',','.');
             string sql = "Insert into Produto (nome,descricao,categoria,modelo,marca,valor,quantidade,inativo) " +
-               $"VALUES ('{p.nome}','{p.descricao}','{p.categoria}','{p.modelo}','{p.marca}','{p.valor}','{p.quantidade}','0');";
-
+               $"VALUES ('{p.nome}','{p.descricao}','{p.categoria}','{p.modelo}','{p.marca}','{valorPonto}','{p.quantidade}','0');";
+            
             using (SqlConnection sqlConn = Conexao.getInstancia().getConexaoSql())
             {
                 sqlConn.Open();
@@ -64,6 +64,7 @@ namespace WebServiceE.DAL
         internal string alterarProduto(Produt p)
         {
             string retorno = "0";
+            string valorPonto = p.valor.ToString().Replace(',', '.');
             StringBuilder sb = new StringBuilder();
             sb.Append("UPDATE Produto SET ");
 
@@ -71,7 +72,7 @@ namespace WebServiceE.DAL
             sb.Append("marca = '" + p.marca + "',");
             sb.Append("modelo = '" + p.modelo + "',");
             sb.Append("quantidade = '" + p.quantidade + "',");
-            sb.Append("valor = '" + p.valor + "',");
+            sb.Append("valor = '" + valorPonto + "',");
             sb.Append("descricao = '" + p.descricao + "',");
             sb.Append("categoria = '" + p.categoria + "'");
 
